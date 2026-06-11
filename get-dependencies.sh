2385e4a448799711bc2a0a1defe089b86956b162
Link4Electronics <link4electronics@gmail.com>
2026-06-11 09:52:07 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,11 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    libsoup3 \
-    webkit2gtk-4.1
+pacman -Syu --noconfirm libsoup3
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano webkit2gtk-4.1-mini
 
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
