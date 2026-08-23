e97fc60c35bf42cd65b23735aaecb4f249976a06
Link4Electronics <link4electronics@gmail.com>
2026-08-23 18:53:01 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -7,6 +7,7 @@ export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export ICON=https://raw.githubusercontent.com/caoccao/BetterMediaInfo/refs/heads/main/src-tauri/icons/128x128%402x.png
 export DEPLOY_GTK=1
 export GTK_DIR=gtk-3.0
 export DEPLOY_OPENGL=1
